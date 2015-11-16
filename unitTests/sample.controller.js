1e05bbd35611df0f21cb88edf81a559b93be09dc
Tomasz Szewcow <tomasz.szewcow@capgemini.com>
2015-11-16 10:58:47 +0100

    config + simple controller test (as TDD)

--- unitTests/sample.controller.js
+++ unitTests/sample.controller.js
@@ -1,5 +1,16 @@
-angular.module('sampleModule').controller('sampleController', function(){
-   'use strict';
+angular.module('sampleModule').controller('SampleController', function ($scope) {
+    'use strict';
 
+    $scope.factorial = function (n) {
+        if (n < 0 || isNaN(n)){
+            throw new Error('incorrect argument');
+        }
+        if (n === 0) {
+            return 1;
+        }
+        else {
+            return n * $scope.factorial(n - 1);
+        }
+    };
 
 });
--- unitTests/sample.controller.spec.js
+++ unitTests/sample.controller.spec.js
@@ -1,16 +1,39 @@
-describe('dsadsa', function () {
+describe('SampleController tests', function () {
     'use strict';
 
     var $scope;
 
     beforeEach(module('sampleModule'));
     beforeEach(inject(function ($controller, $rootScope) {
-
+        $scope = $rootScope.$new();
+        $controller('SampleController', {$scope: $scope});
     }));
 
-    it('should compare 2 to 2', function () {
-        expect(2).toBe(2);
-
+    it('should return 1 for 0!', function () {
+        expect($scope.factorial(0)).toBe(1);
     });
 
+    it('should return 120 for 5!', function(){
+        expect($scope.factorial(5)).toBe(120);
+    });
+
+    it('should return 24 for 4!', function(){
+        expect($scope.factorial(4)).toBe(24);
+    });
+
+    it('should return 1 fro 1!', function(){
+        expect($scope.factorial(1)).toBe(1);
+    });
+
+    it('should throw exception for negative numbers', function(){
+       expect(function(){
+           $scope.factorial(-1);
+       }).toThrow(new Error('incorrect argument'));
+    });
+
+    it('should throw exception for not-a-number args', function(){
+        expect(function(){
+            $scope.factorial('not-a-number');
+        }).toThrow(new Error('incorrect argument'));
+    });
 });
